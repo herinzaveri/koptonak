dbab68c18e8e184c506551b4cee385a5f1914245
chaitanya rana <chaitanyarana0204@gmail.com>
2021-05-13 19:54:58 +0530

    updated app.module.ts to work live

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,11 +7,12 @@ import { AppRoutingModule, routingComponents } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MaterialModule } from "./material/material.module";
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
 	declarations: [AppComponent, routingComponents],
 	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule, FormsModule, ReactiveFormsModule, HttpClientModule],
-	providers: [],
+	providers: [ {provide: LocationStrategy, useClass: HashLocationStrategy}],
 	bootstrap: [AppComponent],
 })
 export class AppModule {}
